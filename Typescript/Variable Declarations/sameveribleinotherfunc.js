34f8f937e7a0a99ac60c03e4fb5fffefff50f960
Sarwar Sikder <Sarwar Sikder>
2016-05-10 12:51:43 +0600

    First Push

---
+++ Typescript/Variable Declarations/sameveribleinotherfunc.js
@@ -0,0 +1,11 @@
+function f() {
+    var a = 10;
+    return function g() {
+        var b = a + 1;
+        return b;
+    };
+}
+var g = f();
+console.log(g());
+g(); // returns 11;
+//# sourceMappingURL=sameveribleinotherfunc.js.map
